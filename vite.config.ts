615763ac4708ade44eeeac88075d09eb5f4caa71
Maulikpokiya74 <maulikpokiya7475@gmail.com>
2026-01-14 16:11:14 +0530

    fix: api issue

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://meesho-backend-s2wx.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
